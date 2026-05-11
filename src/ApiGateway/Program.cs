30eeb64cb4c166d35d1d80c1f14ea69ab13bfb60
uma-ram <jananijishnumar@gmail.com>
2026-05-11 11:57:12 +0100

    Implemented ratelimitoption in .json file

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,17 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:80");
 
-builder.Services.AddControllers();
+//Load ocelot.json config
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
 
 
 var app = builder.Build();
 
-
-app.UseAuthorization();
-app.MapControllers();
-
 // Health check endpoint — Docker Compose uses this
 app.MapGet("/health", () => Results.Ok(new { status = "healthy" }));
+
+await app.UseOcelot();
 
 app.Run();
